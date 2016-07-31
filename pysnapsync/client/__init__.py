361e89fb92d89e8529fcdb11e2ac920b3abea237
David Taylor <davidt@yadt.uk>
2016-07-31 19:58:55 +0100

    Initial commit

---
+++ pysnapsync/client/__init__.py
@@ -0,0 +1,20 @@
+# coding=utf-8
+"""PySnapSync client.
+
+This package implements the pysnapsync client.
+
+The package exports the following modules:
+
+o `snapsync` main backup script.
+
+See the module doc strings for more information.
+"""
+
+
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import division
+
+from pysnapsync.client import snapsync
+
+__all__ = [snapsync]
---
+++ pysnapsync/tests/test_dummy.py
@@ -0,0 +1,23 @@
+# coding=utf-8
+"""Dummy test.
+
+Pointless dummy test.
+"""
+
+
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import division
+
+# import pysnapsync.server
+# import pysnapsync.client
+
+
+def inc(arg):
+    """Return arg incremented by one."""
+    return arg + 1
+
+
+def test_answer():
+    """Assert 3+1 == 4."""
+    assert inc(3) == 4
